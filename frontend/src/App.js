e7178fb36fdf3bcf5bf3682bbd4dadf7b9e1e989
Hamza ARROUBAT <ht.arroubat@gmail.com>
2021-09-16 15:48:26 +0100

    frontend/added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import Charts from "pages/Charts";
+import Home from "pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route } from 'react-router';
 
 export default function App() {
   return (
-    <div>
-      <h1>Gaming Charts</h1>
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/charts">
+          <Charts />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
