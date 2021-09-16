e1fa58cd0df0aa5a084c6c7f9592ab8bfa277a9a
Hamza ARROUBAT <ht.arroubat@gmail.com>
2021-09-16 09:10:04 +0100

    backend/init

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
