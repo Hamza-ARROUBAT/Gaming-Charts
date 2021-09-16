cb07cbeee7545c8e6916aff872338b41b178b56e
Hamza ARROUBAT <ht.arroubat@gmail.com>
2021-09-16 12:59:47 +0100

    backend/topByplayers(done)

--- backend/api/controllers/selectController.js
+++ backend/api/controllers/selectController.js
@@ -13,6 +13,19 @@ const topByPlaytime = (req, res) => {
     .catch((err) => console.error(err));
 };
 
+const topByPlayers = (req, res) => {
+  const { genre, platform } = req.query;
+
+  getGames(genre, platform)
+    .then((games) => {
+      const sorted = _.sortBy(games, ['totalPlayers']).reverse();
+      const topGames = sorted.slice(0, 3);
+      res.json(topGames);
+    })
+    .catch((err) => console.error(err));
+};
+
 module.exports = {
   topByPlaytime,
+  topByPlayers,
 };
--- backend/api/routers/select.js
+++ backend/api/routers/select.js
@@ -4,5 +4,6 @@ const selectController = require('../controllers/selectController');
 const router = express.Router();
 
 router.get('/select_top_by_playtime', selectController.topByPlaytime);
+router.get('/select_top_by_players', selectController.topByPlayers);
 
 module.exports = router;
